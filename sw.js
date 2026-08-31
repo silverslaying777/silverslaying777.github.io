b0a52fcd03fe592861aeb01a7e22d95e58bac9d9
brightpatch <drflick@gmail.com>
2026-08-30 22:26:36 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+/* Bright Patch offline cache. Bump VERSION whenever you upload a new index.html. */
+var VERSION = "bright-patch-v9";
+var FILES = ["./", "./index.html", "./app.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open(VERSION).then(function(c){ return c.addAll(FILES); }).then(function(){ return self.skipWaiting(); }));
+});
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k!==VERSION; }).map(function(k){ return caches.delete(k); }));
+  }).then(function(){ return self.clients.claim(); }));
+});
+self.addEventListener("fetch", function(e){
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      var copy = res.clone();
+      if (new URL(e.request.url).origin === location.origin) caches.open(VERSION).then(function(c){ c.put(e.request, copy); });
+      return res;
+    }).catch(function(){ return caches.match(e.request); })
+  );
+});
